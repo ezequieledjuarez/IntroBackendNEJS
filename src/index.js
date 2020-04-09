6a9648d0eed49666f2c6053e67a62ecb9a5053b8
ezzeguason <ezze.juarezz@gmail.com>
2020-04-09 08:46:48 -0300

    Rutas.S2C6.Error al importar como funcion, funcionó importarlo como objeto. Perdida de legibilidad.Revisar

--- src/index.js
+++ src/index.js
@@ -1,61 +1,10 @@
 const express = require('express');
-
-const { info, error } = require('./modules/my_log');
-const { countries } = require('countries-list');
+const rutas = require('./routes/index.js');
 
 const app = express();
 
-app.get('/', function(request, response) {
-    response.send('Hola');
-});
+rutas.routes(app);
 
-app.get('/info', function(request, response) {
-    info('Hola info');
-    response.send('Info');
-});
-
-app.get('*', function(request, response) {
-    response.status(404).send('Error 404 Pagina no encontrada');
-});
-
-// var server = http.createServer(function(request, response) {
-//     var parsed = url.parse(request.url);
-//     console.log("parsed:", parsed);
-
-//     var pathname = parsed.pathname;
-
-//     var query = querystring.parse(parsed.query);
-//     console.log("query", query);
-
-//     if (pathname === "/") {
-//         response.writeHead(200, { "Content-Type": "text/html" });
-//         response.write("<html><body><p>HOME PAGE</p></body></html>");
-//         response.end();
-//     } else if (pathname === "/exit") {
-//         response.writeHead(200, { "Content-Type": "text/html" });
-//         response.write("<html><body><p>BYE</p></body></html>");
-//         response.end();
-//     } else if (pathname === "/country") {
-//         response.writeHead(200, { "Content-Type": "application/json" });
-//         response.write(JSON.stringify(countries[query.code]));
-//         response.end();
-//     } else if (pathname === "/info") {
-//         var result = info(pathname);
-//         response.writeHead(200, { "Content-Type": "text/html" });
-//         response.write(result);
-//         response.end();
-//     } else if (pathname === "/error") {
-//         var result = error(pathname);
-//         response.writeHead(200, { "Content-Type": "text/html" });
-//         response.write(result);
-//         response.end();
-//     } else {
-//         response.writeHead(404, { "Content-Type": "text/html" });
-//         response.write("<html><body><p>NOT FOUND</p></body></html>");
-//         response.end();
-//     }
-// });
-
-app.listen(4000, function() {
+app.listen(4000, () => {
     console.log('running on 4000');
 });
--- src/modules/my_log.js
+++ src/modules/my_log.js
@@ -1,14 +1,14 @@
-  function info(text) {
-      console.log("INFO:", text);
-      return text;
-  }
+const info = (text) => {
+    console.log('INFO:', text);
+    return text;
+};
 
-  function error(text) {
-      console.log("ERROR:", text);
-      return text;
-  }
+const error = (text) => {
+    console.log('ERROR:', text);
+    return text;
+};
 
-  module.exports.info = info;
-  module.exports.error = error;
-  //EXPORTACIÓN GLOBAL
-  // module.exports = { info, error };
+module.exports.info = info;
+module.exports.error = error;
+//EXPORTACIÓN GLOBAL
+// module.exports = { info, error };
